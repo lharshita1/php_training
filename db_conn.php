5733e8daa9db8d36a9121831b70afb02a17ca0be
Harshita Luthra <75129029+lharshita1@users.noreply.github.com>
2023-04-27 21:10:36 +0530

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,11 @@
+<?php
+
+    $sname = "localhost";
+    $uname = "root";
+    $password = "";
+    $db_name ="test_db1";
+    $conn = mysqli_connect($sname,$uname,$password,$db_name);
+
+    if(!$conn){
+        echo "Connection Failed";
+    }
